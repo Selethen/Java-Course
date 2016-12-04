acc07a634c19326103f6469b518127b9632c2a16
Lukasz Parulski <lukaszparulski@wp.pl>
2016-12-04 17:41:15 +0100

    Dodanie programu do zwracania przednajwiększej liczby

---
+++ Java Course/src/randomProgramy/Program.java
@@ -0,0 +1,41 @@
+package randomProgramy;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Podaj liczby: ");
+		String allNumbers = sc.nextLine();
+		List<String> numbers = new ArrayList<>();
+		numbers = Arrays.asList(allNumbers.split("\\s"));
+		List<Integer> indeedNumbers = new ArrayList<>();
+		for(String numberAsString: numbers) {
+			indeedNumbers.add(Integer.parseInt(numberAsString));
+		}
+		System.out.println(highestButOne(indeedNumbers));
+	}
+
+	public static Integer highestButOne(List<Integer> list) {
+		Collections.sort(list);
+		Collections.reverse(list);
+		int i;
+		if(list.size()==2)
+			return list.get(1);
+		else {
+		for(i=0; i<list.size()-1; i++) {
+			if( list.get(i) != list.get(i+1) ) {
+				return list.get(i+1);
+			}
+				
+		}
+		}
+		return null;
+	}
+}
+
