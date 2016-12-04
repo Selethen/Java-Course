5beeaac307cb7e23ddb5d2f5fe2856ff2a493a28
Lukasz Parulski <lukaszparulski@wp.pl>
2016-12-04 18:27:54 +0100

    Kosmetyczne poprawki kodu

--- Java Course/src/randomProgramy/Program.java
+++ Java Course/src/randomProgramy/Program.java
@@ -6,6 +6,10 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/*Program do zwracania
+przednajwiekszej z podanych
+liczb calkowitych.*/
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -15,27 +19,29 @@ public class Program {
 		List<String> numbers = new ArrayList<>();
 		numbers = Arrays.asList(allNumbers.split("\\s"));
 		List<Integer> indeedNumbers = new ArrayList<>();
-		for(String numberAsString: numbers) {
+		for (String numberAsString : numbers) {
 			indeedNumbers.add(Integer.parseInt(numberAsString));
 		}
 		System.out.println(highestButOne(indeedNumbers));
 	}
 
+	/*
+	 * Funkcja zwracajaca przednajwieksza liczbe z listy.
+	 */
 	public static Integer highestButOne(List<Integer> list) {
 		Collections.sort(list);
 		Collections.reverse(list);
 		int i;
-		if(list.size()==2)
+		if (list.size() == 2)
 			return list.get(1);
 		else {
-		for(i=0; i<list.size()-1; i++) {
-			if( list.get(i) != list.get(i+1) ) {
-				return list.get(i+1);
+			for (i = 0; i < list.size() - 1; i++) {
+				if (list.get(i) != list.get(i + 1)) {
+					return list.get(i + 1);
+				}
+
 			}
-				
-		}
 		}
 		return null;
 	}
 }
-
