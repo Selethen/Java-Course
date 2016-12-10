c08837b5b4b9a0bcf111e11d817c0b4054922f59
Lukasz Parulski <lukaszparulski@wp.pl>
2016-12-10 23:41:42 +0100

    Kosmetyczne poprawki

--- Java Course/src/randomProgramy/Program.java
+++ Java Course/src/randomProgramy/Program.java
@@ -9,14 +9,13 @@ import java.util.List;
 /**
  * @author Lukasz Parulski
  *
- * Program do zwracania przednajwiekszej liczby calkowitej z podanych
+ *         Program do zwracania przednajwiekszej liczby calkowitej z podanych
  * 
  */
 public class Program {
-	
+
 	/**
 	 * Funkcja main
-	 * @return
 	 */
 	public static void main(String[] args) {
 		String stringNumbers = in();
@@ -24,35 +23,35 @@ public class Program {
 		Integer number = highestButOne(numbers);
 		out(number);
 	}
-	
+
 	/**
 	 * Funkcja przyjmuje liczby z wejscia i wpisuje je do listy
 	 * 
-	 * @return
+	 * @return Zwraca ci¹g znaków, w którym s¹ liczby oddzielone spacjami
 	 */
 	public static String in() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Podaj liczby: ");
 		String allNumbers = sc.nextLine();
-		return allNumbers;	
+		return allNumbers;
 	}
-	
+
 	/**
 	 * Funkcja zamienia stringa na listê intów
 	 * 
 	 * @param allNumbers
-	 * @return
+	 * @return Zwraca listê liczb ca³kowitych
 	 */
-	public static List<Integer> parseList(String allNumbers){
+	public static List<Integer> parseList(String allNumbers) {
 		List<String> numbers = new ArrayList<>();
 		numbers = Arrays.asList(allNumbers.split("\\s"));
 		List<Integer> indeedNumbers = new ArrayList<>();
-		for (String numberAsString: numbers) {
+		for (String numberAsString : numbers) {
 			indeedNumbers.add(Integer.parseInt(numberAsString));
 		}
 		return indeedNumbers;
 	}
-	
+
 	/**
 	 * Funkcja wypisuje przednajwieksza liczbe z podanych
 	 * 
@@ -62,27 +61,27 @@ public class Program {
 		System.out.println("Przednajwieksza liczba z podanych to: ");
 		System.out.println(number);
 	}
-	
+
 	/**
 	 * Funkcja zwraca przednajwieksza z podanej listy liczb
 	 * 
 	 * @param list
 	 * 
-	 * @return
+	 * @return Zwraca przednajwiêksz¹ liczbê z listy, lub null je¿eli przednajwiêksza liczba nie istnieje
 	 */
 	public static Integer highestButOne(List<Integer> list) {
 		Collections.sort(list);
 		Collections.reverse(list);
 		int i;
-		if (list.size()==2)
+		if (list.size() == 2)
 			return list.get(1);
 		else {
-		for (i = 0; i < list.size() - 1; i++) {
-			if (list.get(i) != list.get(i + 1)) {
-				return list.get(i + 1);
-			}
+			for (i = 0; i < list.size() - 1; i++) {
+				if (list.get(i) != list.get(i + 1)) {
+					return list.get(i + 1);
+				}
 
-		}
+			}
 		}
 		return null;
 	}
